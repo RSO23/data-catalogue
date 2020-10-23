1c5b671f5bdcaa2f532fb9b42d450da3752a7087
Blarc <jakob.malezic@gmail.com>
2020-10-23 11:51:19 +0200

    create basic project structure

---
+++ src/main/java/rso/datacatalogue/DataCatalogueApplication.java
@@ -0,0 +1,13 @@
+package rso.datacatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rso/datacatalogue/DataCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package rso.datacatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
