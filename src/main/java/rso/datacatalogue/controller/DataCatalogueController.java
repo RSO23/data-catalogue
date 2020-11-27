12ddfe299c76337d8913e03cbf8e4b3a3e4a3bd7
Blarc <jakob.malezic@gmail.com>
2020-11-27 19:29:18 +0100

    added info.json, endpoint for basic info and changed workflow to DockerHub

--- src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
+++ src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
@@ -1,5 +1,11 @@
 package rso.datacatalogue.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,25 @@ public class DataCatalogueController
     @GetMapping("/ping")
     public String ping() {
         return "Pong!";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        ClassPathResource resource = new ClassPathResource("/static/info.json");
+
+        String result = "";
+
+        try {
+            InputStream inputStream = resource.getInputStream();
+
+            result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
