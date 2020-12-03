b659be4650042d951c2068702a438f19980ceed7
Blarc <jakob.malezic@gmail.com>
2020-12-03 19:21:12 +0100

    added consul discovery and cleaned up poms

--- src/main/java/rso/datacatalogue/DataCatalogueApplication.java
+++ src/main/java/rso/datacatalogue/DataCatalogueApplication.java
@@ -2,8 +2,10 @@ package rso.datacatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DataCatalogueApplication {
 
 	public static void main(String[] args) {
