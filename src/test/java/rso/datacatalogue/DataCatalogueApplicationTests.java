c36c6e374b95a1e0c0c1d4a34b59394a826b206c
Blarc <jakob.malezic@gmail.com>
2020-12-05 16:29:40 +0100

    fixed github workflow

--- src/test/java/rso/datacatalogue/DataCatalogueApplicationTests.java
+++ src/test/java/rso/datacatalogue/DataCatalogueApplicationTests.java
@@ -3,7 +3,7 @@ package rso.datacatalogue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest({"spring.cloud.consul.enabled=false"})
 class DataCatalogueApplicationTests {
 
 	@Test
