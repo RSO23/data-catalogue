8971c5ed25273c854931b426b8efb3253cc2763c
Blarc <jakob.malezic@gmail.com>
2020-12-06 19:02:14 +0100

    added centralized logging with logit.io

--- src/main/java/rso/datacatalogue/DataCatalogueApplication.java
+++ src/main/java/rso/datacatalogue/DataCatalogueApplication.java
@@ -1,9 +1,13 @@
 package rso.datacatalogue;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -12,6 +16,12 @@ public class DataCatalogueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataCatalogueApplication.class, args);
+	}
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void contextRefreshEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		ApplicationContext applicationContext = contextRefreshedEvent.getApplicationContext();
+		MDC.put("applicationName", applicationContext.getId());
 	}
 
 }
--- src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
+++ src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import rso.datacatalogue.config.ConfigProperties;
 @RequiredArgsConstructor
 public class DataCatalogueController
 {
+    private final Logger log = LoggerFactory.getLogger(DataCatalogueController.class);
     private final ConfigProperties configProperties;
 
     @GetMapping("/ping")
@@ -28,7 +31,7 @@ public class DataCatalogueController
         return configProperties.getTestConfig();
     }
 
-    @GetMapping("/info")
+    @GetMapping(value = "/info", produces = "application/json")
     public String info() {
         ClassPathResource resource = new ClassPathResource("/static/info.json");
 
@@ -42,7 +45,7 @@ public class DataCatalogueController
         }
         catch (IOException e)
         {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
 
         return result;
