2d962c4e88c43b4dd8d2efb2a9c5bad0036a8dc1
Blarc <jakob.malezic@gmail.com>
2020-12-08 19:34:11 +0100

    added swagger

--- src/main/java/rso/datacatalogue/DataCatalogueApplication.java
+++ src/main/java/rso/datacatalogue/DataCatalogueApplication.java
@@ -1,17 +1,31 @@
 package rso.datacatalogue;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableSwagger2
 public class DataCatalogueApplication {
 
 	public static void main(String[] args) {
@@ -23,5 +37,34 @@ public class DataCatalogueApplication {
 		ApplicationContext applicationContext = contextRefreshedEvent.getApplicationContext();
 		MDC.put("applicationName", applicationContext.getId());
 	}
+
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("rso.datacatalogue"))
+				.paths(PathSelectors.any())
+				.build()
+				.securitySchemes(List.of(apiKey()))
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Data catalogue API",
+				"This is a private API for League of Legends predictor application",
+				"1.0",
+				"Students license",
+				new Contact("Jakob Maležič", "https://github.com/Blarc", "jm6421@student.uni-lj.si"),
+				"API License",
+				"https://google.com",
+				Collections.emptyList()
+		);
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+
 
 }
--- src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
+++ src/main/java/rso/datacatalogue/controller/DataCatalogueController.java
@@ -8,14 +8,19 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import rso.datacatalogue.config.ConfigProperties;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "Basic operations", produces = "application/json", consumes = "application/json")
 public class DataCatalogueController
 {
     private final Logger log = LoggerFactory.getLogger(DataCatalogueController.class);
