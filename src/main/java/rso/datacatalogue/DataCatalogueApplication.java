dedb912f885ba559f86c3a78e479e416c54d9951
Blarc <jakob.malezic@gmail.com>
2020-12-09 15:21:00 +0100

    added kubernetes dependencies

--- src/main/java/rso/datacatalogue/DataCatalogueApplication.java
+++ src/main/java/rso/datacatalogue/DataCatalogueApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +27,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableSwagger2
+@EnableDiscoveryClient
 public class DataCatalogueApplication {
 
 	public static void main(String[] args) {
